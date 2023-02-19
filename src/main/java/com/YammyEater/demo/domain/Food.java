33b494a55086272d082b7dd4e53b21d274b1719d
osm <osm0822@naver.com>
2023-02-19 15:25:28 +0900

    refactor(domain) : 1.Food에 builder추가, 2.setter를 필요한 곳에만 적용

--- src/main/java/com/YammyEater/demo/domain/Food.java
+++ src/main/java/com/YammyEater/demo/domain/Food.java
@@ -16,52 +16,66 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="FOOD")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Getter
-@Setter
+@Table(name="FOOD")
 public class Food {
     @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name = "FOOD_ID")
     private Long id;
 
     //음식 이름
+    @Setter
     @Column(name = "NAME")
     private String name;
 
     //소개글 제목
+    @Setter
     @Column(name = "TITLE")
     private String title;
 
     //리뷰 평균 평점
+    @Setter
     @Column(name = "RATING")
     private float rating;
 
     //음식 종류(레시피 = RECIPE, 완제품 = PRODUCT, 배달 = DELIVERY)
+    @Setter
     @Column(name = "TYPE")
     @Enumerated(value = EnumType.STRING)
     private FoodType type;
 
     //음식 재료 설명(닭, 배추 등)
+    @Setter
     @Column(name = "INGREDIENT")
     private String ingredient;
 
     //가격
+    @Setter
     @Column(name = "PRICE")
     private Long price;
 
     //회사(배달음식이나 완제품의 경우 회사명 필요)
+    @Setter
     @Column(name = "MAKER")
     private String maker;
 
     //대표이미지 경로
+    @Setter
     @Column(name = "IMG_URL")
     private String imgUrl;
 
     //영양소
+    @Setter
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "FOOD_ID")
     private Nutrient nutrient;
@@ -71,14 +85,17 @@ public class Food {
     private List<FoodTag> tags = new ArrayList<>();
 
     //작성자
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID")
     private User writer;
 
+    @Setter
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "FOOD_ID")
     private Article article;
 
+    @Setter
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "FOOD_ID")
     private FoodReviewRatingCount foodReviewRatingCount;
