049ca3e92997e3592fa70a15539c08fa4f6ff303
osm <osm0822@naver.com>
2023-03-24 01:22:54 +0900

    feat : jwt 토큰 발행 서비스 작성. 인증 필터 등록. passwordEncoder 등록

---
+++ src/main/java/com/YammyEater/demo/security/JwtAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.YammyEater.demo.security;
+
+import com.YammyEater.demo.service.JwtTokenProvider;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        athenticate(request);
+        filterChain.doFilter(request, response);
+    }
+
+    private void athenticate(HttpServletRequest request) {
+        //토큰 추출, 없으면 인증 종료
+        String accessToken = parseBearerToken(request);
+        if(accessToken == null) {
+            return;
+        }
+        //검증 후 userId 추출, 없으면 인증 종료
+        Long userId = jwtTokenProvider.validateAndGetUserId(accessToken);
+        if(userId == null) {
+            return;
+        }
+        //인증 성공
+        //SecurityContextHolder에 저장
+        AbstractAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                userId,
+                null,
+                AuthorityUtils.NO_AUTHORITIES
+        );
+        authenticationToken.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authenticationToken);
+        SecurityContextHolder.setContext(securityContext);
+
+    }
+
+    private String parseBearerToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/YammyEater/demo/service/JwtTokenProvider.java
@@ -0,0 +1,51 @@
+package com.YammyEater.demo.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+
+@Service
+public class JwtTokenProvider {
+    @Value("${jwt.secret_key}")
+    private String SECRET_KEY;
+
+    @Value("${jwt.access_token_expire_minute}")
+    private int accessTokenExpireMinute;
+
+    public String create(Long userId) {
+        Date now = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        calendar.add(Calendar.MINUTE, accessTokenExpireMinute);
+        Date expiredAt = calendar.getTime();
+
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                .setSubject(String.valueOf(userId))
+                .setIssuer("YammyEater")
+                .setIssuedAt(now)
+                .setExpiration(expiredAt)
+                .compact();
+    }
+
+    public Long validateAndGetUserId(String token) {
+
+        Claims claims;
+        try {
+            claims = Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (JwtException jwtException) {
+            return null;
+        }
+
+        return Long.parseLong(claims.getSubject());
+    }
+}
