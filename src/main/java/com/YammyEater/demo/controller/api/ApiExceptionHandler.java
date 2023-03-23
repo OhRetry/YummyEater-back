0e1cdbac1796f05c57c545aadb15dd389b451a1f
osm <osm0822@naver.com>
2023-03-24 01:05:36 +0900

    feat : api 기본 예외 처리 구현

---
+++ src/main/java/com/YammyEater/demo/controller/api/ApiExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.YammyEater.demo.controller.api;
+
+import com.YammyEater.demo.constant.ErrorCode;
+import com.YammyEater.demo.dto.ApiResponse;
+import com.YammyEater.demo.exception.GeneralException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> general(GeneralException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getErrorCode(), request);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> internalServerError(Exception e, WebRequest request) {
+
+        return handleExceptionInternal(e, ErrorCode.INTERNAL_SERVER_ERROR, request);
+    }
+
+    //스프링 기본 지원 에러처리의 body에 ApiResponse를 넣어주기 위함.
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex, ErrorCode.valueOf(status), headers, status, request);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(Exception ex, ErrorCode errorCode, WebRequest request) {
+        return handleExceptionInternal(ex, errorCode, HttpHeaders.EMPTY, errorCode.getHttpStatus(), request);
+    }
+
+    private ResponseEntity<Object> handleExceptionInternal(Exception ex, ErrorCode errorCode, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        return super.handleExceptionInternal(
+                ex,
+                ApiResponse.of(null, errorCode),
+                headers,
+                status,
+                request
+        );
+    }
+}
---
+++ src/main/java/com/YammyEater/demo/exception/GeneralException.java
@@ -0,0 +1,19 @@
+package com.YammyEater.demo.exception;
+
+import com.YammyEater.demo.constant.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class GeneralException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public GeneralException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+    public GeneralException(ErrorCode errorCode) {
+        this(errorCode, errorCode.getMessage());
+    }
+}
+
