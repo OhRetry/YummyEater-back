7786782c90407010ca20a975c6ae77e86aeca2eb
osm <osm0822@naver.com>
2023-03-24 01:10:35 +0900

    feat : 메일 서비스 구현

---
+++ src/main/java/com/YammyEater/demo/service/MailServiceImpl.java
@@ -0,0 +1,39 @@
+package com.YammyEater.demo.service;
+
+import com.YammyEater.demo.constant.ErrorCode;
+import com.YammyEater.demo.exception.GeneralException;
+import com.sun.mail.util.MailConnectException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailPreparationException;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailServiceImpl implements MailService {
+    @Autowired
+    private JavaMailSender emailSender;
+
+
+    public void sendEmail(String sendTo, String title, String content) {
+        try {
+            final MimeMessagePreparator preparator = mimeMessage -> {
+                final MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+                helper.setTo(sendTo);
+                helper.setSubject(title);
+                helper.setText(content);
+            };
+            emailSender.send(preparator);
+        }
+        catch (MailSendException e) {
+            return;
+        }
+        catch (MailException e) {
+            throw new GeneralException(ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
