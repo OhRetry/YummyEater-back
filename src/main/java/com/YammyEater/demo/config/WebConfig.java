2e2845455473eb2eb197f7d1f19a857ad73b1360
osm <osm0822@naver.com>
2023-05-07 20:06:35 +0900

    config : 기본 cors 설정에서 허용되지 않던 delete, put, patch에 대해서 허용하도록 변경.

--- src/main/java/com/YammyEater/demo/config/WebConfig.java
+++ src/main/java/com/YammyEater/demo/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.YammyEater.demo.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,7 +19,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.PATCH.name()
+                        );
     }
 
     @Override
