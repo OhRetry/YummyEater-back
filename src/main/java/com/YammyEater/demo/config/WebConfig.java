bf31dd9399e35a28ca10e5b02090796de9addfbc
osm <osm0822@naver.com>
2023-09-22 01:54:38 +0900

    config: cors 대상 주소를 환경변수로부터 받도록 변경

--- src/main/java/com/YammyEater/demo/config/WebConfig.java
+++ src/main/java/com/YammyEater/demo/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.YammyEater.demo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${FRONTEND_HOST}")
+    private String FRONTEND_HOST;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(FRONTEND_HOST)
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
