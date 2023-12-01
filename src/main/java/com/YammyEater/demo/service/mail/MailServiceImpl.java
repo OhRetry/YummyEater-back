b63b7ea8c6ccc291f2fdea78927d90b5be1eea59
osm <osm0822@naver.com>
2023-12-01 23:46:04 +0900

    feat: 이메일 전송 메서드를 비동기로 변경

--- src/main/java/com/YammyEater/demo/service/mail/MailServiceImpl.java
+++ src/main/java/com/YammyEater/demo/service/mail/MailServiceImpl.java
@@ -3,20 +3,27 @@ package com.YammyEater.demo.service.mail;
 import com.YammyEater.demo.constant.error.ErrorCode;
 import com.YammyEater.demo.exception.GeneralException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MailServiceImpl implements MailService {
 
     private final JavaMailSender emailSender;
+    private final Logger logger = LoggerFactory.getLogger(MailServiceImpl.class);
 
+    @Async
     public void sendEmail(String sendTo, String title, String content) {
         try {
             final MimeMessagePreparator preparator = mimeMessage -> {
@@ -28,9 +35,11 @@ public class MailServiceImpl implements MailService {
             emailSender.send(preparator);
         }
         catch (MailSendException e) {
+            logger.info("이메일 전송 실패", e);
             return;
         }
         catch (MailException e) {
+            logger.error("이메일 서비스 에러", e);
             throw new GeneralException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
     }
